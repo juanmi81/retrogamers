4e1b3972883e8ba8224a4d8ee2c44ac27c9e43a6
juanmi81 <101145721+juanmi81@users.noreply.github.com>
2022-04-16 00:34:16 +0200

    disparos
    Se ha programado disparos, se dispara con la tecla control derecha del teclado o el izquierdo del raton

--- Assets/Movimiento.cs
+++ Assets/Movimiento.cs
@@ -4,8 +4,15 @@ using UnityEngine;
 
 public class Movimiento : MonoBehaviour {
 
+    [Header("Movimiento")]
     public float speed;
     private Rigidbody rig;
+
+    [Header("Armas")]
+    public GameObject Disparo;
+    public Transform shotSpawn;
+    public float fireRate;
+    private float nextFire;
 
     void Start() {
 
@@ -15,6 +22,12 @@ public class Movimiento : MonoBehaviour {
 
    
     void Update() {
+
+        if (Input.GetButton("Fire1") && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            Instantiate(Disparo, shotSpawn.position, shotSpawn.rotation);
+        }
 
         float movimientohorizontal = Input.GetAxis("Horizontal");
 
@@ -27,4 +40,6 @@ public class Movimiento : MonoBehaviour {
 
 
     }
+
+
 }
