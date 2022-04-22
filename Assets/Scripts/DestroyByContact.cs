f0b2e38eb917eac276f15a324fbfa3925581fea2
d4nib <dblancoalb@uoc.edu>
2022-04-22 22:31:48 +0200

    Revert "l"
    This reverts commit 935edc481598723aaf99112d7ca83a1de0351e84.

--- Assets/Scripts/DestroyByContact.cs
+++ Assets/Scripts/DestroyByContact.cs
@@ -2,22 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
-public class DestroybyContact : MonoBehaviour {
-
+public class DestroyByContact : MonoBehaviour
+{
     public GameObject explosion;
-      
-    
+
+
     void OnTriggerEnter(Collider other)
     {
-
+        // Instantiate(explosion, transform.position, transform.rotation);
         if (other.CompareTag("limite")) return;
-
-        Instantiate(explosion, transform.position, transform.rotation);
-  
-        
         Destroy(other.gameObject);
         Destroy(gameObject);
-        
     }
 }
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 
@@ -13,14 +12,9 @@ public class GameController : MonoBehaviour
     public float startWait;
     public float waveWait;
 
-    private int puntuacion;
-    public Text texto;
-
     // Start is called before the first frame update
     void Start()   
     {
-        puntuacion = 0;
-        UpdateScore();
         StartCoroutine(SpawnWaves());
     }
 
@@ -43,17 +37,5 @@ public class GameController : MonoBehaviour
         
         }
         
-    }
-
-    public void Añadirpuntos (int value) 
-    {
-        puntuacion += value;
-        UpdateScore();
-
-    }
-
-    void UpdateScore ()
-    {
-        texto.text = "Puntuacion: " + puntuacion;
     }
 }
